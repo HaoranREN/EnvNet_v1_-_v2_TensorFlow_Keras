16f9ccb0f435da2e3b9133fc7ab94ab0543492ef
Haoran Ren <31661918+HaoranREN@users.noreply.github.com>
2020-06-06 19:36:29 -0400

    Code Upload

---
+++ EnvNet_v1.py
@@ -0,0 +1,182 @@
+'''
+File: EnvNet_v1.py
+Author: Haoran Ren
+Email: rhaoran1@umbc.edu
+Github: https://github.com/HaoranREN/EnvNet_v2_TensorFlow_Keras
+
+An implementation of EnvNet v2 in Python with TensorFlow
+Trained on ESC-50 dataset
+
+This file contains the main function
+
+EnvNet_v1:
+
+@inproceedings{tokozume2017learning,
+  title={Learning environmental sounds with end-to-end convolutional neural network},
+  author={Tokozume, Yuji and Harada, Tatsuya},
+  booktitle={2017 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
+  pages={2721--2725},
+  year={2017},
+  organization={IEEE}
+}
+
+ESC-50:
+
+https://github.com/karolpiczak/ESC-50
+
+@inproceedings{piczak2015esc,
+  title={ESC: Dataset for environmental sound classification},
+  author={Piczak, Karol J},
+  booktitle={Proceedings of the 23rd ACM international conference on Multimedia},
+  pages={1015--1018},
+  year={2015}
+}
+'''
+
+import EnvNet_v1_data_utils as utils
+
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, Conv2D, BatchNormalization, Activation
+from tensorflow.keras.layers import MaxPooling1D, MaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import InputLayer, Reshape, Flatten, Dense, Dropout
+from tensorflow.keras import initializers, optimizers
+from tensorflow.keras.callbacks import LearningRateScheduler, ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.backend import sum, argmax, get_value
+
+
+def build_model(class_num):
+
+    # If changing input shape, change pooling layers and the reshape layer accordingly
+    # Refer EnvNet paper for details on pooling size
+    
+    model = Sequential()
+    
+    model.add(InputLayer(input_shape = (24000,1)))
+    
+    model.add(Conv1D(filters=40, kernel_size=8, padding = 'same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(Conv1D(filters=40, kernel_size=8, padding = 'same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling1D(pool_size=160))
+    model.add(Reshape((150,40,1)))
+    
+    model.add(Conv2D(filters=50, kernel_size=(13,8)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(3,3)))
+
+    model.add(Conv2D(filters=50, kernel_size=(5,1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    
+    model.add(MaxPooling2D(pool_size=(3,1)))
+
+    model.add(Flatten())
+    
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(class_num, activation = 'softmax'))
+    
+    return model
+    
+
+def lr_schedule(epoch):
+
+    lr = 1e-2
+    if epoch > 120:
+        lr *= 1e-3
+    elif epoch > 100:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+
+if __name__ == '__main__':
+    
+    class_num = 50
+    batch_size = 16
+    
+    x_train, y_train, x_val, y_val, x_test, y_test = utils.split_dataset()
+    
+    train_sequence = utils.Train_Sequence(x_train, y_train, batch_size)
+    val_sequence = utils.Val_Sequence(x_val, y_val, batch_size)
+    #test_sequence = utils.Val_Sequence(x_test, y_test, batch_size)
+    
+    model = build_model(class_num)    
+    
+    lr_scheduler = LearningRateScheduler(lr_schedule)
+
+    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                   cooldown=0,
+                                   patience=3,
+                                   min_lr=1e-6)
+                                                      
+    earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='auto')
+
+    model.compile(loss='categorical_crossentropy', 
+                  optimizer=optimizers.SGD(lr=lr_schedule(0), momentum=0.6, nesterov=True), 
+                  metrics=["accuracy"])
+              
+    model.summary()
+
+    callbacks = [lr_scheduler, lr_reducer]
+    
+    history = model.fit_generator(train_sequence,
+                                  steps_per_epoch=int(np.ceil(x_train.shape[0] / batch_size)),
+                                  epochs=150,
+                                  verbose=1,
+                                  callbacks=callbacks,
+                                  validation_data=val_sequence,
+                                  validation_steps=int(np.ceil(x_val.shape[0] / batch_size)),
+                                  use_multiprocessing = True,
+                                  workers = 2)
+    
+    '''
+    loss, acc = model.evaluate_generator(test_sequence,
+                                         steps = int(np.ceil(x_test.shape[0] / batch_size)),
+                                         verbose=1)
+    '''
+    
+    # sliding-windows testing with probability voting
+    correct = 0
+    test_set_size = len(x_test)
+    
+    confusion_matrix = np.zeros((51,51), dtype = np.int32)
+    confusion_matrix[0,1:] = np.arange(50)
+    confusion_matrix[1:,0] = np.arange(50).reshape(1,50)
+    
+    for i in range(test_set_size):
+    
+        if i % 50 == 0:
+            print('Testing on ' + str(i + 1) + ' of ' + str(test_set_size) + ' samples.')
+    
+        sliding_windows = utils.sliding_windows_for_testing(x_test[i])
+        
+        pred = get_value(argmax(sum(model.predict_on_batch(sliding_windows), axis = 0)))
+        label = np.argmax(y_test[i])
+        
+        confusion_matrix[label + 1, pred + 1] += 1
+        
+        if pred == label:
+            correct += 1
+    
+    acc = correct / test_set_size
+    print('Test accuracy: ' + str(acc))
+    
+    np.savetxt('confusion_matrix.csv', confusion_matrix, fmt="%d", delimiter=",")
+    
+    model.save('model.h5')
+    
---
+++ EnvNet_v1_data_utils.py
@@ -0,0 +1,230 @@
+'''
+File: EnvNet_v1_data_utils.py
+Author: Haoran Ren
+Email: rhaoran1@umbc.edu
+Github: https://github.com/HaoranREN/EnvNet_v2_TensorFlow_Keras
+
+An implementation of EnvNet v2 in Python with TensorFlow
+Trained on ESC-50 dataset
+
+This file contains data preprocessing functions
+
+EnvNet_v1:
+
+@inproceedings{tokozume2017learning,
+  title={Learning environmental sounds with end-to-end convolutional neural network},
+  author={Tokozume, Yuji and Harada, Tatsuya},
+  booktitle={2017 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
+  pages={2721--2725},
+  year={2017},
+  organization={IEEE}
+}
+
+ESC-50:
+
+https://github.com/karolpiczak/ESC-50
+
+@inproceedings{piczak2015esc,
+  title={ESC: Dataset for environmental sound classification},
+  author={Piczak, Karol J},
+  booktitle={Proceedings of the 23rd ACM international conference on Multimedia},
+  pages={1015--1018},
+  year={2015}
+}
+'''
+
+import numpy as np
+import random
+import math
+
+import librosa
+from scipy import signal
+
+from tensorflow.keras.utils import Sequence
+
+DATA_DIR = 'path_to_ESC_50'
+AUDIO_DIR = 'path_to_ESC_50/audio/'
+
+AUDIO_SR = 16000        # sampling rate
+AUDIO_WINDOW = 24000    # audio window size
+WINDOW_STRIDE = 3200    # windowsing stride
+AUDIO_LENGTH = 80000
+CLASS_NUM = 50
+
+
+
+###### Load train, val, and test subsets lists ######
+
+def label_categorical(class_num = CLASS_NUM):
+    
+    label_cat = {}
+    
+    for i in range(class_num):
+        tmp = np.zeros(class_num)
+        tmp[i] = 1
+        label_cat[i] = tmp
+    
+    return label_cat
+    
+
+def split_dataset():
+
+    label_cat = label_categorical(CLASS_NUM)
+    
+    with open(DATA_DIR + 'meta/esc50.csv') as f:
+        lines  = f.readlines()[1:]
+    
+    random.shuffle(lines)
+    
+    x_train = []
+    y_train = []
+    x_val = []
+    y_val = []
+    x_test = []
+    y_test = []
+    
+    for line in lines:
+        columns = line.strip().split(',')
+        
+        # use default 5-fold cross-validation
+        
+        # val
+        if columns[1] == '5':
+            x_val.append(AUDIO_DIR + columns[0])
+            y_val.append(label_cat[int(columns[2])])
+            
+            
+        #test
+        elif columns[1] == '4':
+            x_test.append(AUDIO_DIR + columns[0])
+            y_test.append(label_cat[int(columns[2])])
+
+            
+        # train
+        else:
+            x_train.append(AUDIO_DIR + columns[0])
+            y_train.append(label_cat[int(columns[2])])
+
+    return np.array(x_train), np.array(y_train), np.array(x_val), np.array(y_val), np.array(x_test), np.array(y_test)
+
+    
+    
+###### Audio preprocessing ######
+
+def random_window(wave, window_size=AUDIO_WINDOW, eps=1e-12):
+
+    wave_min = np.min(wave)
+    wave_max = np.max(wave)
+    wave = (wave - wave_min) / (wave_max - wave_min + eps) * 2 - 1
+    
+    # filter out silent window with maximum amplitude smaller than 0.2
+    window_min = 0
+    window_max = 0
+    
+    while window_min > -0.2 and window_max < 0.2:
+        start = np.random.choice(len(wave) - window_size)
+        idx = np.arange(window_size) + start
+        #wave = wave[start: start + window_size]
+        window = wave[idx]
+        window_min = np.min(window)
+        window_max = np.max(window)
+        window = (window - window_min) / (window_max - window_min + eps) * 2 - 1
+
+    return window
+
+    
+
+def train_augment(file_name):
+
+    wave = librosa.core.load(file_name, sr=AUDIO_SR)[0]
+    tensor = random_window(wave).reshape(AUDIO_WINDOW, 1)
+
+    return tensor
+
+
+def val_augment(file_name):
+
+    wave = librosa.core.load(file_name, sr=AUDIO_SR)[0]
+    tensor = random_window(wave).reshape(AUDIO_WINDOW, 1)
+
+    return tensor
+
+
+###### Data generator (keras.Sequence class) ######
+
+class Train_Sequence(Sequence):
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        return np.array([train_augment(file_name) for file_name in batch_x]), np.array(batch_y)
+
+
+class Val_Sequence(Sequence):
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        return np.array([val_augment(file_name) for file_name in batch_x]), np.array(batch_y)
+
+
+def sliding_windows_for_testing(file_name, eps=1e-12):
+    wave = librosa.core.load(file_name, sr=AUDIO_SR)[0]
+    wave_min = np.min(wave)
+    wave_max = np.max(wave)
+    wave = (wave - wave_min) / (wave_max - wave_min + eps) * 2 - 1
+    
+    windows_num = ((wave.shape[0] - AUDIO_WINDOW) // WINDOW_STRIDE) + 1
+    windows = wave[WINDOW_STRIDE * np.arange(windows_num)[:, None] + np.arange(AUDIO_WINDOW)].reshape(windows_num, AUDIO_WINDOW, 1)
+    
+    # filter out silent window with maximum amplitude smaller than 0.2
+    windows = np.delete(windows, np.where(np.max(np.absolute(windows), axis = 1) < 0.2)[0], axis = 0)
+    
+    windows_min = np.min(windows, axis = 1)
+    windows_max = np.max(windows, axis = 1)
+    
+    windows = (windows[:,:,0] - windows_min) / (windows_max - windows_min + eps) * 2 - 1
+    window_shape = windows.shape
+    windows = windows.reshape(window_shape[0], window_shape[1], 1)
+    
+    return windows
+
+      
+if __name__ == '__main__':
+    
+    x_train, y_train, x_val, y_val, x_test, y_test = split_dataset()
+    
+    print(len(x_train))
+    print(len(x_val))
+    print(len(x_test))
+    print(x_train[0])
+    print(y_train[0])
+    
+    
+    print(librosa.core.load(x_train[0], sr=AUDIO_SR)[0].shape)
+    print(train_augment(x_train[0]).shape)
+    print(np.min(train_augment(x_train[0])))
+    print(np.max(train_augment(x_train[0])))
+    print(type(train_augment(x_train[0])))
+    
+    file = x_test[0]
+    print(sliding_windows_for_testing(file).shape)
+    print(np.min(sliding_windows_for_testing(file), axis = 1))
+    print(np.max(sliding_windows_for_testing(file), axis = 1))
+    print(type(sliding_windows_for_testing(file)))
